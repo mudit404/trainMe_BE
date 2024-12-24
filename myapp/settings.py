0d69da0ea05ee74e605b399064131e6f8fc567f6
mudit404 <guptamudit8910@gmail.com>
2024-12-25 01:46:38 +0530

    changes to settings

--- myapp/settings.py
+++ myapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-n&iuaf9f*apwdbql_r)3u9m9wl%**htvnn^=+k0l=t@hr7*_=e"
+SECRET_KEY = os.environ.get("django-insecure-n&iuaf9f*apwdbql_r)3u9m9wl%**htvnn^=+k0l=t@hr7*_=e")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "http://localhost:5174/",
+
+]
 
 
 # Application definition
@@ -82,11 +86,16 @@ WSGI_APPLICATION = "myapp.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "empty",
-        "USER": "postgres",
-        "PASSWORD": "1234",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.environ.get("empty"),
+        "USER": os.environ.get("postgres"),
+        "PASSWORD": os.environ.get("1234"),
+        "HOST": os.environ.get("localhost"),
+        "PORT": os.environ.get("5432"),
+        # "NAME": "empty",
+        # "USER": "postgres",
+        # "PASSWORD": "1234",
+        # "HOST": "localhost",
+        # "PORT": "5432",
     }
 }
 
